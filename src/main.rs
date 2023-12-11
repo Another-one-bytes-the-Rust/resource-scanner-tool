6b1dca410f75676363ca6aef61c2cbc9625ae5e9
Kirgnition <riccardobuah@gmail.com>
2023-12-11 13:16:48 +0100

    Added custom MapCoordinate struct, changed scan function return type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 pub mod resource_scanner {
+    use std::error::Error;
     use robotics_lib::interface::{Tools, robot_map, robot_view, discover_tiles, one_direction_view};
     use robotics_lib::runner::Runnable;
     use robotics_lib::world::coordinates::Coordinate;
@@ -8,6 +9,7 @@ pub mod resource_scanner {
     use robotics_lib::utils::LibError;
 
     pub struct ResourceScanner {}
+    impl Tools for ResourceScanner {}
 
     /// Represents different scanning patterns used in the resource scanner tool.
     ///
@@ -73,41 +75,128 @@ pub mod resource_scanner {
         DiagonalStar(usize)
     }
 
+    pub struct MapCoordinate {
+        width: usize,
+        height: usize
+    }
+    impl MapCoordinate {
+        /// Creates a new `MapCoordinate` instance with the given width and height.
+        ///
+        /// # Arguments
+        ///
+        /// * `width` - The width of the map.
+        /// * `height` - The height of the map.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// let coordinates = MapCoordinate::new(10, 20);
+        /// ```
+        pub fn new(width: usize, height: usize) -> Self {
+            MapCoordinate { width, height }
+        }
 
+        /// Gets the width of the map coordinate.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// let coordinates = MapCoordinate::new(10, 20);
+        /// let width = coordinates.get_width();
+        /// assert_eq!(width, 10);
+        /// ```
+        pub fn get_width(&self) -> usize {
+            self.width
+        }
 
-    impl Tools for ResourceScanner {}
+        /// Sets the width of the map coordinate.
+        ///
+        /// # Arguments
+        ///
+        /// * `width` - The new width value.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// let mut coordinates = MapCoordinate::new(10, 20);
+        /// coordinates.set_width(15);
+        /// assert_eq!(coordinates.get_width(), 15);
+        /// ```
+        pub fn set_width(&mut self, width: usize) {
+            self.width = width;
+        }
+
+        /// Gets the height of the map coordinate.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// let coordinates = MapCoordinate::new(10, 20);
+        /// let height = coordinates.get_height();
+        /// assert_eq!(height, 20);
+        /// ```
+        pub fn get_height(&self) -> usize {
+            self.height
+        }
+
+        /// Sets the height of the map coordinate.
+        ///
+        /// # Arguments
+        ///
+        /// * `height` - The new height value.
+        ///
+        /// # Example
+        ///
+        /// ```
+        /// let mut coordinates = MapCoordinate::new(10, 20);
+        /// coordinates.set_height(25);
+        /// assert_eq!(coordinates.get_height(), 25);
+        /// ```
+        pub fn set_height(&mut self, height: usize) {
+            self.height = height;
+        }
+    }
+
 
     impl ResourceScanner {
         /// The scan function scans an area around the robot for the required content according to the pattern.
         /// it returns an option containing tile coordinates and number of contents if some content is found.
-        /// if no content is found in the given area it returns the origin as coordinate and 0 as quantity.
-        /// if the robot didn't have enough energy it returns None
+        /// if no content is found in the given area it returns None
+        /// if the robot didn't have enough energy an Error
+        ///
+        /// # Energy Cost
+        ///
         pub fn scan(&mut self,
                     world: &World,
                     robot: &mut impl Runnable,
                     pattern: Pattern,
                     content: Content
-        ) -> Option<(Coordinate, usize)> {
+        ) -> Result<Option<(MapCoordinate, usize)>,Box<dyn Error>> {
             // first check if any of the tiles in the scan pattern are already present in the robot map
-            let coordinates_to_check: Vec<Coordinate> = match pattern {
+            let coordinates_to_check: Vec<MapCoordinate> = match pattern {
                 Pattern::Area(size) => {
                     //todo check if size is odd, if not return an error
+                    if size % 2 == 0 || size < 3 {
+                        return Err(Box::new())
+                    }
+
                     let tile_map = robot_view(robot,world);
-                    let out = Vec::new();
+                    let (robot_row, robot_col) = (robot.get_coordinate().get_row(), robot.get_coordinate().get_col());
+                    let mut out = Vec::new();
 
                     for (height_idx,row_vec) in tile_map.iter().enumerate() {
                         for (width_idx, tile) in row_vec.iter().enumerate() {
-                            out.push(Coordinate::new())
+                            out.push(MapCoordinate::new(width_idx,height_idx))
                         }
                     }
-
+                    out
                 }
                 _ => Vec::new()
-            }
+            };
         }
-
-
     }
 }
 
-fn main() {}
+fn main() {
+
+}
