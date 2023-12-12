c1ad6ebb2c8d67dff12ec66f1bcb61845bad2668
Kirgnition <riccardobuah@gmail.com>
2023-12-12 12:19:36 +0100

    moved pattern struct back to main

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,77 @@ pub mod resource_scanner {
     use robotics_lib::world::tile::{Content, Tile};
     use robotics_lib::world::World;
     use robotics_lib::utils::LibError;
-    use resource_scanner_tool::map_coordinate::{MapCoordinate, Pattern};
+    use resource_scanner_tool::map_coordinate::MapCoordinate;
     use resource_scanner_tool::tool_errors::ToolError;
     use resource_scanner_tool::tool_errors::ToolError::*;
 
+    /// Represents different scanning patterns used in the resource scanner tool.
+    ///
+    /// The `Pattern` enum is used to specify the scanning behavior, and each variant
+    /// includes a `usize` field indicating the range or size of the scan.
+    ///
+    /// # Variants
+    ///
+    /// - `Area(usize)`: Scans in a square area with a side length specified by the `usize` parameter.
+    /// - `DirectionUp(usize)`: Scans in an upward direction with the specified distance.
+    /// - `DirectionRight(usize)`: Scans in a rightward direction with the specified distance.
+    /// - `DirectionLeft(usize)`: Scans in a leftward direction with the specified distance.
+    /// - `DirectionDown(usize)`: Scans in a downward direction with the specified distance.
+    /// - `DiagonalUpperLeft(usize)`: Scans diagonally in the upper-left direction with the specified distance.
+    /// - `DiagonalUpperRight(usize)`: Scans diagonally in the upper-right direction with the specified distance.
+    /// - `DiagonalLowerLeft(usize)`: Scans diagonally in the lower-left direction with the specified distance.
+    /// - `DiagonalLowerRight(usize)`: Scans diagonally in the lower-right direction with the specified distance.
+    /// - `StraightStar(usize)`: Scans in a star pattern in all directions with the specified distance.
+    /// - `DiagonalStar(usize)`: Scans in a star pattern diagonally in all directions with the specified distance.
+    ///
+    /// ASCII drawing for `StraightStar(2)`:
+    ///
+    /// ```plaintext
+    ///      *
+    ///      *
+    ///    **r**
+    ///      *
+    ///      *
+    /// ```
+    ///
+    /// ASCII drawing for `DiagonalStar(3)`:
+    ///
+    /// ```plaintext
+    ///  *     *
+    ///   *   *
+    ///    * *
+    ///     r
+    ///    * *
+    ///   *   *
+    ///  *     *
+    /// ```
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// // Scan in a square area with a side length of 5.
+    /// use resource_scanner_tool::map_coordinate::Pattern;
+    /// let area_scan = Pattern::Area(5);
+    ///
+    /// // Scan upward with a distance of 3.
+    /// let up_scan = Pattern::DirectionUp(3);
+    /// ```
+    pub enum Pattern {
+        Area(usize),
+        DirectionUp(usize),
+        DirectionRight(usize),
+        DirectionLeft(usize),
+        DirectionDown(usize),
+        DiagonalUpperLeft(usize),
+        DiagonalUpperRight(usize),
+        DiagonalLowerLeft(usize),
+        DiagonalLowerRight(usize),
+        StraightStar(usize),
+        DiagonalStar(usize)
+    }
+
     pub struct ResourceScanner {}
     impl Tools for ResourceScanner {}
-
-
-
 
     impl ResourceScanner {
         /// The scan function scans an area around the robot for the required content according to the pattern.
