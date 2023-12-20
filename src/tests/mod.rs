ab8a82383fb08e22b7e3aa4cdede1552cf10475a
Kirgnition <riccardobuah@gmail.com>
2023-12-20 20:36:41 +0100

    added tests for area scan

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,21 +1,21 @@
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use std::mem;
+    use crate::coordinates::map_coordinate::MapCoordinate;
+    use crate::errors::tool_errors::ToolError;
+    use crate::tool::resource_scanner::{Pattern, ResourceScanner};
     use robotics_lib::energy::Energy;
     use robotics_lib::event::events::Event;
-    use robotics_lib::interface::debug;
-    use robotics_lib::runner::{Robot, Runnable, Runner};
+    use robotics_lib::interface::{go, robot_map, Direction};
     use robotics_lib::runner::backpack::BackPack;
+    use robotics_lib::runner::{Robot, Runnable, Runner};
     use robotics_lib::world::coordinates::Coordinate;
     use robotics_lib::world::environmental_conditions::EnvironmentalConditions;
     use robotics_lib::world::environmental_conditions::WeatherType::Sunny;
     use robotics_lib::world::tile::{Content, Tile, TileType};
-    use robotics_lib::world::World;
     use robotics_lib::world::world_generator::Generator;
     use robotics_lib::world::world_generator::World as WorldType;
-    use crate::coordinates::map_coordinate::MapCoordinate;
-    use crate::errors::tool_errors::ToolError;
-    use crate::tool::resource_scanner::{Pattern, ResourceScanner};
+    use robotics_lib::world::World;
 
     #[test]
     fn test_new_map_coordinate() {
@@ -85,16 +85,12 @@ mod tests {
 
         let converted_tuple: (usize, usize) = coordinates.into();
         assert_eq!(converted_tuple, (10, 20));
-
     }
 
     #[test]
     fn test_debug_display_and_error_traits() {
         // Test Debug trait
-        assert_eq!(
-            format!("{:?}", ToolError::InvalidSizeError),
-            "Invalid Size"
-        );
+        assert_eq!(format!("{:?}", ToolError::InvalidSizeError), "Invalid Size");
         assert_eq!(
             format!("{:?}", ToolError::EmptyCoordinates),
             "Empty Coordinates"
@@ -113,10 +109,7 @@ mod tests {
         );
 
         // Test Display trait
-        assert_eq!(
-            format!("{}", ToolError::InvalidSizeError),
-            "Invalid Size"
-        );
+        assert_eq!(format!("{}", ToolError::InvalidSizeError), "Invalid Size");
         assert_eq!(
             format!("{}", ToolError::EmptyCoordinates),
             "Empty Coordinates"
@@ -135,47 +128,56 @@ mod tests {
         );
 
         // Test Error trait
-        assert_eq!(
-            ToolError::InvalidSizeError.to_string(),
-            "Invalid Size"
-        );
-        assert_eq!(
-            ToolError::EmptyCoordinates.to_string(),
-            "Empty Coordinates"
-        );
-        assert_eq!(
-            ToolError::NotEnoughEnergy.to_string(),
-            "Not Enough Energy"
-        );
-        assert_eq!(
-            ToolError::NoMoreDiscovery.to_string(),
-            "No More Discovery"
-        );
+        assert_eq!(ToolError::InvalidSizeError.to_string(), "Invalid Size");
+        assert_eq!(ToolError::EmptyCoordinates.to_string(), "Empty Coordinates");
+        assert_eq!(ToolError::NotEnoughEnergy.to_string(), "Not Enough Energy");
+        assert_eq!(ToolError::NoMoreDiscovery.to_string(), "No More Discovery");
         assert_eq!(
             ToolError::Other("Custom Error".to_string()).to_string(),
             "Custom Error"
         );
     }
 
-    fn test_tool_with(size: usize, spawn_x: usize, spawn_y: usize) {
+    #[test]
+    fn test_scan_tool_area_3() {
         struct TestRobot(Robot);
         impl Runnable for TestRobot {
             fn process_tick(&mut self, world: &mut World) {
-                let info =debug(self, world);
-                let mut tool = ResourceScanner{};
-                let result = tool.scan(world,self,Pattern::StraightStar(2),Content::Coin(3));
-                match result {
-                    Ok(r) => {
-                        match r {
-                            Some(max_tile) => {
-                                println!("max_tile: coord=({},{}) n={}",max_tile.0.get_width(), max_tile.0.get_height(),max_tile.1)
-                            }
-                            None => {
-                                println!("No tile found")
-                            }
+                let mut tool = ResourceScanner {};
+                // path the robot must follow
+                let directions = [
+                    Direction::Right,
+                    Direction::Down,
+                    Direction::Right,
+                    Direction::Down,
+                    Direction::Up,
+                    Direction::Right,
+                    Direction::Up,
+                    Direction::Right,
+                ];
+                // expected results from the scan tool
+                let expected_results = [
+                    None,
+                    None,
+                    Some((MapCoordinate::new(2, 2), 1)),
+                    Some((MapCoordinate::new(2, 2), 1)),
+                    Some((MapCoordinate::new(2, 2), 1)),
+                    Some((MapCoordinate::new(2, 2), 1)),
+                    Some((MapCoordinate::new(2, 2), 1)),
+                    None,
+                ];
+
+                for (phase, dir) in directions.iter().enumerate() {
+                    let result = tool.scan(world, self, Pattern::Area(3), Content::Coin(0));
+                    match result {
+                        Ok(content) => {
+                            assert_eq!(expected_results[phase], content);
                         }
+                        Err(_) => panic!(),
                     }
-                    Err(error) => println!("{}",error)
+
+                    //move the robot
+                    go(self, world, dir.to_owned()).expect("");
                 }
             }
             fn handle_event(&mut self, event: Event) {
@@ -207,12 +209,17 @@ mod tests {
             size: usize,
             spawn_x: usize,
             spawn_y: usize,
-            tile_type: TileType
+            tile_type: TileType,
         }
 
-        impl WorldGenerator{
+        impl WorldGenerator {
             fn new(size: usize, spawn_x: usize, spawn_y: usize, tile_type: TileType) -> Self {
-                Self {size,spawn_x,spawn_y, tile_type}
+                Self {
+                    size,
+                    spawn_x,
+                    spawn_y,
+                    tile_type,
+                }
             }
         }
 
@@ -223,28 +230,175 @@ mod tests {
                 for _ in 0..self.size {
                     let mut row: Vec<Tile> = Vec::new();
                     for _ in 0..self.size {
-                        let tile = Tile{
+                        let tile = Tile {
                             tile_type: self.tile_type,
-                            content: Content::Coin(3),
-                            elevation: 0
+                            content: Content::None,
+                            elevation: 0,
                         };
                         row.push(tile);
                     }
                     map.push(row);
                 }
+                // add coin in (2,2)
+                map[2][2] = Tile {
+                    tile_type: self.tile_type,
+                    content: Content::Coin(1),
+                    elevation: 0,
+                };
 
-                let environmental_conditions = EnvironmentalConditions::new(&vec![Sunny], 15, 12).unwrap();
+                let environmental_conditions =
+                    EnvironmentalConditions::new(&vec![Sunny], 15, 12).unwrap();
                 // implementation
-                return (map, (self.spawn_x, self.spawn_y), environmental_conditions, 10.0, None);
+                return (
+                    map,
+                    (self.spawn_x, self.spawn_y),
+                    environmental_conditions,
+                    10.0,
+                    None,
+                );
             }
         }
 
         let r = TestRobot(Robot::new());
-        let runner = Runner::new(Box::new(r),&mut WorldGenerator::new(size,spawn_x,spawn_y,TileType::Grass));
+        let  runner = Runner::new(
+            Box::new(r),
+            &mut WorldGenerator::new(5, 0, 0, TileType::Grass),
+        );
+        let _ = runner.unwrap().game_tick();
+
+    }
+
+    #[test]
+    fn test_scan_tool_area_5() {
+        struct TestRobot(Robot);
+        impl Runnable for TestRobot {
+            fn process_tick(&mut self, world: &mut World) {
+                let mut tool = ResourceScanner {};
+                // path the robot must follow
+                let directions = [
+                    Direction::Right,
+                    Direction::Down,
+                    Direction::Right,
+                    Direction::Down,
+                    Direction::Up,
+                    Direction::Right,
+                    Direction::Up,
+                    Direction::Right,
+                ];
+                // expected results from the scan tool
+                let expected_results = [
+                    None,
+                    None,
+                    Some((MapCoordinate::new(2, 3), 1)),
+                    None,
+                    None,
+                    None,
+                    None,
+                    None,
+                ];
+
+                for (phase, dir) in directions.iter().enumerate() {
+                    let result = tool.scan(world, self, Pattern::Area(5), Content::Coin(0));
+                    let _known = robot_map(world);
+                    match result {
+                        Ok(content) => {
+                            assert_eq!(expected_results[phase], content);
+                        }
+                        Err(e) => panic!("{}", e),
+                    }
+
+                    //move the robot
+                    go(self, world, dir.to_owned()).expect("");
+                }
+            }
+            fn handle_event(&mut self, event: Event) {
+                println!();
+                println!("{:?}", event);
+                println!();
+            }
+            fn get_energy(&self) -> &Energy {
+                &self.0.energy
+            }
+            fn get_energy_mut(&mut self) -> &mut Energy {
+                &mut self.0.energy
+            }
+            fn get_coordinate(&self) -> &Coordinate {
+                &self.0.coordinate
+            }
+            fn get_coordinate_mut(&mut self) -> &mut Coordinate {
+                &mut self.0.coordinate
+            }
+            fn get_backpack(&self) -> &BackPack {
+                &self.0.backpack
+            }
+            fn get_backpack_mut(&mut self) -> &mut BackPack {
+                &mut self.0.backpack
+            }
+        }
+
+        struct WorldGenerator {
+            size: usize,
+            spawn_x: usize,
+            spawn_y: usize,
+            tile_type: TileType,
+        }
+
+        impl WorldGenerator {
+            fn new(size: usize, spawn_x: usize, spawn_y: usize, tile_type: TileType) -> Self {
+                Self {
+                    size,
+                    spawn_x,
+                    spawn_y,
+                    tile_type,
+                }
+            }
+        }
+
+        impl Generator for WorldGenerator {
+            fn gen(&mut self) -> WorldType {
+                let mut map: Vec<Vec<Tile>> = Vec::new();
+                // Initialize the map with default tiles
+                for _ in 0..self.size {
+                    let mut row: Vec<Tile> = Vec::new();
+                    for _ in 0..self.size {
+                        let tile = Tile {
+                            tile_type: self.tile_type,
+                            content: Content::None,
+                            elevation: 0,
+                        };
+                        row.push(tile);
+                    }
+                    map.push(row);
+                }
+                // add coin in (2,2)
+                map[2][3] = Tile {
+                    tile_type: self.tile_type,
+                    content: Content::Coin(1),
+                    elevation: 0,
+                };
+
+                let environmental_conditions =
+                    EnvironmentalConditions::new(&vec![Sunny], 15, 12).unwrap();
+
+                // implementation
+                return (
+                    map,
+                    (self.spawn_x, self.spawn_y),
+                    environmental_conditions,
+                    10.0,
+                    None,
+                );
+            }
+        }
+
+        let r = TestRobot(Robot::new());
+        let runner = Runner::new(
+            Box::new(r),
+            &mut WorldGenerator::new(50, 0, 0, TileType::Grass),
+        );
         let _ = runner.unwrap().game_tick();
     }
-    #[test]
-    fn test_scan_tool() {
-        test_tool_with(100,0,0);
-    }
+
+    //todo() test scan function with Content::Fire,Bin since they have different values
+    //todo() test not enough energy
 }
